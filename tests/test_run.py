52ef129e363a13089d448c12b94e0d0580e7b049
Eivind Fonn <evfonn@gmail.com>
2020-06-18 15:26:09 +0200

    Add test for template files

--- tests/test_run.py
+++ tests/test_run.py
@@ -7,6 +7,7 @@ from badger import Case
 
 DATADIR = Path(__file__).parent / 'data'
 
+
 def test_echo():
     case = Case(DATADIR / 'run' / 'echo.yaml')
     case.clear_cache()
@@ -20,6 +21,27 @@ def test_echo():
     assert data['c'].dtype == float
     assert data['charlie'].dtype == int
     assert data['echo'].dtype == float
+    np.testing.assert_array_equal(data['a'], [[1, 1, 1], [2, 2, 2], [3, 3, 3]])
+    np.testing.assert_array_equal(data['alpha'], [[1, 1, 1], [2, 2, 2], [3, 3, 3]])
+    np.testing.assert_array_equal(data['b'], [['a', 'b', 'c'], ['a', 'b', 'c'], ['a', 'b', 'c']])
+    np.testing.assert_array_equal(data['bravo'], [['a', 'b', 'c'], ['a', 'b', 'c'], ['a', 'b', 'c']])
+    np.testing.assert_array_equal(data['c'], [[1, 1, 1], [3, 3, 3], [5, 5, 5]])
+    np.testing.assert_array_equal(data['charlie'], [[1, 1, 1], [3, 3, 3], [5, 5, 5]])
+
+
+def test_cat():
+    case = Case(DATADIR / 'run' / 'cat.yaml')
+    case.clear_cache()
+    case.run()
+
+    data = case.result_array()
+    assert data['a'].dtype == int
+    assert data['alpha'].dtype == int
+    assert data['b'].dtype == object
+    assert data['bravo'].dtype == object
+    assert data['c'].dtype == float
+    assert data['charlie'].dtype == int
+    assert data['cat'].dtype == float
     np.testing.assert_array_equal(data['a'], [[1, 1, 1], [2, 2, 2], [3, 3, 3]])
     np.testing.assert_array_equal(data['alpha'], [[1, 1, 1], [2, 2, 2], [3, 3, 3]])
     np.testing.assert_array_equal(data['b'], [['a', 'b', 'c'], ['a', 'b', 'c'], ['a', 'b', 'c']])
